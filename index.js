6eb11f9bdc9d66bb136aa044f465d6b2ba9626d2
Olaoluwa Alabi <Olujuwon@Olaoluwas-MacBook-Pro.local>
2018-02-03 20:53:12 +0200

    code refactored, readme

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ $(document).ready(function(){
             time: $.now()
             });
             window.location.href = "./result.html";
-            //alert("Thank you for your time");
      });
 
      $(".2").click(function(){
@@ -39,38 +38,5 @@ $(document).ready(function(){
                 time: $.now()
             });
             window.location.href = "./result.html";
-     });
-
-     var ref = firebase.database().ref('reviews');
-     ref.on('value', gotData, errData);
-
-     function gotData(data) {
-         //console.log(data.val());
-         var scores = data.val();
-         var keys = Object.keys(scores);
-         //console.log(keys);
-         for (var i=0; i < keys.length; i++) {
-             var k=keys[i];
-             var rv = scores [k].rv;
-             var time = scores [k].time;
-             console.log(rv, time);
-         }
-     }
-     function errData(err) {
-         console.log('Error 404!');
-         console.log(err);
-     }
-
-     //userCountRef.on('child_added', function(snapshot) {
-         //console.log("display database")
-         //$('#displaydata').append("<li id=" + snapshot.val().id + ">" + snapshot.val());
-     //});
-
-
+     });   
 });
-    /*$(".1").on("click", function(){
-        reviews = $("#ex").val()
-        dbRef.ref().set({
-            reviews:reviews
-        });
-    });*/
